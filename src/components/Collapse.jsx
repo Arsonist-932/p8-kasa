7724d9f994f01a0d23bbebc232f2cbd9bd8406bd
Arsonist Tefh <119712506+Arsonist-932@users.noreply.github.com>
2023-12-12 22:00:13 +0100

    Correction composant Collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -3,26 +3,26 @@ import React, { useState } from "react";
 import Arrow from "../assets/images/arrow_collapse.png";
 
 export default function Collapse({ title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [collapse, setIsOpen] = useState(false);
 
   const toggleCollapse = () => {
-    setIsOpen(!isOpen);
+    setIsOpen(!collapse);
   };
 
   return (
     <div className="collapse">
       <div
-        className={`collapse-title ${isOpen ? "open" : ""}`}
+        className={`collapse-title ${collapse ? "open" : ""}`}
         onClick={toggleCollapse}
       >
         {title}
         <img
-          className={`arrow-icon ${isOpen ? "open" : ""}`}
+          className={`arrow-icon ${collapse ? "open" : ""}`}
           src={Arrow}
           alt="Arrow"
         />
       </div>
-      {isOpen && (
+      {collapse && (
         <div className="collapse-desc">
           <p>{text}</p>
         </div>
